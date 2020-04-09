c56bdf0911a7a604a4426524f0306593d12fdb02
bbaktaeho <lth9036@gmail.com>
2020-04-10 01:07:05 +0900

    인터페이스 활용 (디자인패턴:OCP)

---
+++ src/interface/UserDTO.ts
@@ -0,0 +1,5 @@
+export interface UserDTO {
+  email: string;
+  name: string;
+  password: string;
+}
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,6 +1,8 @@
+import { UserDTO } from "../interface/UserDTO";
+
 export default class AuthService {
   constructor() {}
 
-  public async signIn(email: string, password: string): Promise<any> {}
+  public async signIn(user: UserDTO): Promise<any> {}
   public async signUp(): Promise<any> {}
 }
