1e499ce3be2165e360bb45844ed3bfb1aea42622
bbaktaeho <lth9036@gmail.com>
2020-04-12 15:04:00 +0900

    config 배포

---
+++ src/config/db_config.ts
@@ -0,0 +1,40 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+type Config = {
+  username: string;
+  password: string;
+  database: string;
+  host: string;
+  [key: string]: string;
+};
+interface IConfigGroup {
+  development: Config;
+  test: Config;
+  production: Config;
+}
+const dbConfig: IConfigGroup = {
+  development: {
+    username: "root",
+    password: process.env.DB_PASSWORD!,
+    database: "real_chair_database",
+    host: process.env.DB_HOST!,
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: process.env.DB_PASSWORD!,
+    database: "real_chair_database",
+    host: process.env.DB_HOST!,
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: process.env.DB_PASSWORD!,
+    database: "real_chair_database",
+    host: process.env.DB_HOST!,
+    dialect: "mysql",
+  },
+};
+
+export default dbConfig;
