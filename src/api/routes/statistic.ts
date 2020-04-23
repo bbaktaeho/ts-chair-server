48ea490afa1f3301cc8444ecba9d562aa32dc749
bbaktaeho <lth9036@gmail.com>
2020-04-23 17:58:26 +0900

    진짜 최종임

--- src/api/routes/statistic.ts
+++ src/api/routes/statistic.ts
@@ -7,7 +7,7 @@ export default (app: Router) => {
 
   router.get("/date", middlewares.jwtVerify, async (req, res) => {
     const date = req.query.date;
-
+    let time: string = "";
     if (typeof date == "undefined")
       res.status(400).json({ success: false, message: "data is null" });
     else {
@@ -18,14 +18,24 @@ export default (app: Router) => {
         statusCode,
       } = await new StatisticService().showStatistic(req.user!.id, 1, date);
       if (result == null) message = "의자를 사용하지 않았어요!";
-      else message = "성공";
-      res.status(statusCode).json({ success, message, statistics: result });
+      else {
+        message = "성공";
+        if (Array.isArray(result)) {
+          let times: number = result.reduce((a, b) => a + b);
+          let hour = Math.floor(times / 3600);
+          let minute = Math.floor((times % 3600) / 60);
+          time = `${hour} 시간 ${minute} 분`;
+        }
+      }
+      res
+        .status(statusCode)
+        .json({ success, message, statistics: result, time });
     }
   });
 
   router.get("/month", middlewares.jwtVerify, async (req, res) => {
     const date = req.query.date;
-
+    let time: string = "";
     if (typeof date == "undefined")
       res.status(400).json({ success: false, message: "data is null" });
     else {
@@ -36,19 +46,38 @@ export default (app: Router) => {
         statusCode,
       } = await new StatisticService().showStatistic(req.user!.id, 2, date);
       if (result == null) message = "의자를 사용하지 않았어요!";
-      else message = "성공";
-      res.status(statusCode).json({ success, message, statistics: result });
+      else {
+        message = "성공";
+        if (Array.isArray(result)) {
+          let times: number = result.reduce((a, b) => a + b);
+          let hour = Math.floor(times / 3600);
+          let minute = Math.floor((times % 3600) / 60);
+          time = `${hour} 시간 ${minute} 분`;
+        }
+      }
+      res
+        .status(statusCode)
+        .json({ success, message, statistics: result, time });
     }
   });
   router.get("/all", middlewares.jwtVerify, async (req, res) => {
     let message = "";
+    let time: string = "";
     const {
       success,
       result,
       statusCode,
     } = await new StatisticService().showStatistic(req.user!.id, 3, new Date());
     if (result == null) message = "의자를 사용하지 않았어요!";
-    else message = "성공";
-    res.status(statusCode).json({ success, message, statistics: result });
+    else {
+      message = "성공";
+      if (Array.isArray(result)) {
+        let times: number = result.reduce((a, b) => a + b);
+        let hour = Math.floor(times / 3600);
+        let minute = Math.floor((times % 3600) / 60);
+        time = `${hour} 시간 ${minute} 분`;
+      }
+    }
+    res.status(statusCode).json({ success, message, statistics: result, time });
   });
 };
